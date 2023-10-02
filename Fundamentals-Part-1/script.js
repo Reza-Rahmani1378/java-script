903a71819fbd03bbecb84f44bb279d73a8fde738
Reza <reza6273757@gmail.com>
2023-10-02 11:40:29 +0330

    Data Types in JavaScript

--- Fundamentals-Part-1/script.js
+++ Fundamentals-Part-1/script.js
@@ -2,3 +2,11 @@ let js = "amazing";
 if (js == "amazing") 
   alert("JavaScript is FUN!");
 console.log(40 + 8);
+
+let javaScriptisFun = true;
+console.log(javaScriptisFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptisFun);
+console.log(typeof 23);
+console.log(typeof "Reza");
